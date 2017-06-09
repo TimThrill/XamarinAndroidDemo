1ae02d9952f14d49868f0a0e1d0ba4c2b9ad9d6a
cong <macong1989@gmail.com>
2017-06-09 18:59:41 +1000

    Initial version

---
+++ XamarinAndroidDemo/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+
+namespace XamarinAndroidDemo
+{
+    [Activity(Label = "XamarinAndroidDemo", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            EditText username = FindViewById<EditText>(Resource.Id.et_username);
+            EditText passwd = FindViewById<EditText>(Resource.Id.et_passwd);
+
+            Button login = FindViewById<Button>(Resource.Id.bt_login);
+            Button signup = FindViewById<Button>(Resource.Id.bt_sign_up);
+
+            login.Click += Login_Click;
+        }
+
+        private void Login_Click(object sender, System.EventArgs e)
+        {
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.SetTitle("Hi, how are you");
+            alert.Show();
+        }
+    }
+}
+
